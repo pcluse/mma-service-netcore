8cabec2c5f2dcec78f5ebf000893689a4f20dbb7
Johan Dahl <johan.dahl@lth.lu.se>
2021-10-12 09:57:42 +0200

    Rewrote AdminController for new API

--- MMAService/Controllers/AdminController.cs
+++ MMAService/Controllers/AdminController.cs
@@ -1,19 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MMAService.Controllers
 {
-
-    public class AdminRequest
+    public class PrerequisiteReply
     {
-        public string User { get; set; }
-        public string Twofactor { get; set; }
-        public int Expire { get; set; }
+        public string preferredService { get; set; }
+        public string message { get; set; }
     }
 
-    public class AdminReply
+    public class ValidateReply
     {
-        public bool Success { get; set; }
-        public string Message { get; set; }
+        public string message { get; set; }
+        public bool validated { get; set; }
     }
 
     /*
@@ -24,36 +23,39 @@ namespace MMAService.Controllers
     // ControllerBase instead of Controller because minimal framework
     public class AdminController : ControllerBase
     {
-
-        /*
-         * user is filled by MVC when a request is incoming
-         * /check-admin?user=Data
-         * Verify against processlist to see if requesting user has MMA-win open?
-         * Better yet, take the username and domain from the user running the MMA-win process /Robert
-         */
-        [HttpGet("check-admin")]
-        public ActionResult<AdminReply> CheckAdmin([FromQuery(Name = "user")] string user)
+        [HttpGet("/prerequisite/{uid}")]
+        public ActionResult<PrerequisiteReply> GetPrerequisiteUid(string uid)
         {
-            var (success, message) = Program.CheckCanBecomeAdmin(user);
-            return new AdminReply() { Success = success, Message = message };
+            Console.WriteLine("uid {0}", uid);
+            var (success, message) = Program.CheckCanBecomeAdmin(uid);
+            if (!success)
+            {
+                return new PrerequisiteReply() { message = message };
+
+            }
+            return new PrerequisiteReply() { preferredService = message };
         }
 
-        /*
-         * AdminRequest is filled by MVC when a request is incoming
-         * all fields are input from the webrequest
-         */
-        [HttpPost("add-admin")]
-        public ActionResult<AdminReply> AddAdmin(AdminRequest req)
+        [HttpGet("/validate/totp/{uid}/{code}")]
+        public ActionResult<ValidateReply> GetValidateTotpUidCode(string uid, string code)
         {
-            if (req.Expire >= 1 && req.Expire <= 60)
+            var (success, message) = Program.AddAdmin("totp", uid, code);
+            if (!success)
             {
-                var (success, message) = Program.AddAdmin(req.User, req.Twofactor, req.Expire);
-                return new AdminReply() { Success = success, Message = message };
+                return new ValidateReply() { validated = false, message = message };
             }
-            else
+            return new ValidateReply() { validated = true };
+        }
+
+        [HttpGet("/validate/freja/{uid}")]
+        public ActionResult<ValidateReply> GetValidateFrejaUid(string uid)
+        {
+            var (success, message) = Program.AddAdmin("freja", uid, "");
+            if (!success)
             {
-                return new AdminReply() { Success = false, Message = "Length of session is not within range." };
+                return new ValidateReply() { validated = false, message = message };
             }
+            return new ValidateReply() { validated = success };
         }
     }
 }
