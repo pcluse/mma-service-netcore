50760a85feff68157c8deca6014d651a0a7b1fc5
Johan Dahl <johan.dahl@lth.lu.se>
2021-10-12 09:56:58 +0200

    Rewrote Restclient for new API

--- MMAService/RestClient.cs
+++ MMAService/RestClient.cs
@@ -4,121 +4,83 @@ using System.Threading.Tasks;
 
 namespace MMAService
 {
-
-    public class CheckRequest
+    public class PrerequisitesReply
     {
-        public string uid { get; set; }
+
+        public bool PLSLucatLocalAdministrator { get; set; } = false;
+        public bool MLSLucatLocalAdministrator { get; set; } = false;
+        public bool totpActivated { get; set; } = false;
+        public bool frejaActivated { get; set; } = false;
+        public string preferredService { get; set; } = null;
     }
 
-    public class Reply
+    public class ValidateReply
     {
-        public bool response { get; set; } = false;
-    }
-
-    public class VerifyRequest
-    {
-        public string uid { get; set; }
-        public string code { get; set; }
-        public string[] ips = Program.GetLocalIPAddresses();
-        public string hostname = System.Environment.MachineName;
-    }
-
-    public class VerifyReply
-    {
-        public bool response { get; set; }
+        public bool validated { get; set; }
     }
 
     public interface IRestClient
     {
-        Task<bool> CheckLucatAdmin(string user);
-        Task<bool> CheckTFA(string user);
-        Task<bool> Verify(string user, string twofactor);
+        Task<PrerequisitesReply> GetPrerequisites(string username);
+        Task<bool> ValidateFreja(string username);
+        Task<bool> ValidateTotp(string username, string twofactor);
     }
 
     public class RestClient : IRestClient
     {
         private HttpClient client;
-        private HttpClientHandler httpClientHandler;
-        private string ApiKey;
-        private string CertificateThumbprint;
 
-        public RestClient(string baseAddress, string Thumbprint, string InitApiKey)
+        public RestClient(string baseAddress, string apiKey)
         {
-            if (Thumbprint != null)
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-API-KEY", apiKey);
+            if (!baseAddress.EndsWith("/"))
             {
-                CertificateThumbprint = Thumbprint.Replace(":", "").ToUpper();
+                // https://stackoverflow.com/a/23438417
+                baseAddress += "/";
             }
-
-            if (InitApiKey != null)
-            {
-                ApiKey = InitApiKey;
-            }
-            // Define own to allow connecting to site with self signed ssl
-            // Could later be used to control thumprint of certificate
-            httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => {
-                /*
-                 * Certificate validation (Process CRL etc) should be done and after its been validated we check that its the right thumbprint
-                 */
-                if (CertificateThumbprint == null || cert.Thumbprint == CertificateThumbprint)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            };
-            client = new HttpClient(httpClientHandler);
             client.BaseAddress = new Uri(baseAddress);
-            client.Timeout = new TimeSpan(0,0,15);
         }
 
-        private string CleanUsername(string username)
+        private string RemoveDomain(string username)
         {
-            string cleanUsername = "";
-            if (username.Contains("\\"))
+            if (!username.Contains("\\"))
             {
-                cleanUsername = username.Substring(username.IndexOf("\\")+1);
+                return username;
             }
-            else
-            {
-                cleanUsername = username;
-            }
-            return cleanUsername;
-        }
-        private async Task<bool> CallApi(string function,string username,string code = null)
-        {
-            // Api does not use domain, only send username
-            string usernameWithoutDomain = CleanUsername(username);
-            object requestObject = null;
-            if (!String.IsNullOrEmpty(code))
-            {
-                requestObject = new VerifyRequest() { uid = usernameWithoutDomain, code = code };
-            }
-            else
-            {
-                requestObject = new CheckRequest() { uid = usernameWithoutDomain };
-            }
-
-            string RequestUri = String.Format("/api/{0}/?passwd={1}", function, ApiKey);
-            
-            var response = await client.PostAsJsonAsync(RequestUri, requestObject);
-            var responseObject = await response.Content.ReadAsAsync<Reply>();
-            return responseObject.response;
-        }
-        public async Task<bool> CheckLucatAdmin(string user)
-        {
-            return await CallApi("pls_lucat_admin", user);
+            return username.Substring(username.IndexOf("\\") + 1);
         }
 
-        public async Task<bool> CheckTFA(string user)
+        public async Task<PrerequisitesReply> GetPrerequisites(string username)
         {
-            return await CallApi("totp_activated", user);
+            username = RemoveDomain(username);
+            string RequestUri = String.Format("prerequisites/{0}", username);
+
+            var response = await client.GetAsync(RequestUri);
+            response.EnsureSuccessStatusCode();
+            var responseObject = await response.Content.ReadAsAsync<PrerequisitesReply>();
+            return responseObject;
         }
-        public async Task<bool> Verify(string user, string twofactor)
+
+        public async Task<bool> ValidateFreja(string username)
         {
-            return await CallApi("totp_verify", user, twofactor);
+            username = RemoveDomain(username);
+            string RequestUri = String.Format("validate/freja/{0}", username);
+
+            var response = await client.GetAsync(RequestUri);
+            response.EnsureSuccessStatusCode();
+            var responseObject = await response.Content.ReadAsAsync<ValidateReply>();
+            return responseObject.validated;
+        }
+        public async Task<bool> ValidateTotp(string username, string twofactor)
+        {
+            username = RemoveDomain(username);
+            string RequestUri = String.Format("validate/totp/{0}/{1}", username, twofactor);
+
+            var response = await client.GetAsync(RequestUri);
+            response.EnsureSuccessStatusCode();
+            var responseObject = await response.Content.ReadAsAsync<ValidateReply>();
+            return responseObject.validated;
         }
     }
 }
